9736b810003e3a41ce81eb9811890bfb4a9bb4f0
accle <3196385390@qq.com>
2024-06-11 19:50:31 +0800

    part 13

--- spec/spec.rb
+++ spec/spec.rb
@@ -19,7 +19,7 @@ describe 'database' do
   after(:each) do
     FileUtils.rm_f('yangqi_test')
   end
-
+  
   it 'inserts and retrieves a row' do
     result = run_script([
       "insert 1 user1 person1@example.com",
@@ -32,15 +32,6 @@ describe 'database' do
       "Executed.",
       "db > ",
     ])
-  end
-
-  it 'prints error message when table is full' do
-    script = (1..1401).map do |i|
-      "insert #{i} user#{i} person#{i}@example.com"
-    end
-    script << ".exit"
-    result = run_script(script)
-    expect(result[-2]).to eq('db > Error: Table full.')
   end
 
   it 'allows inserting strings that are the maximum length' do
@@ -110,46 +101,6 @@ describe 'database' do
     ])
   end
 
-  it 'prints constants' do
-    script = [
-      ".constants",
-      ".exit",
-    ]
-    result = run_script(script)
-
-    expect(result).to eq([
-      "db > Constants:",
-      "ROW_SIZE: 293",
-      "COMMON_NODE_HEADER_SIZE: 6",
-      "LEAF_NODE_HEADER_SIZE: 10",
-      "LEAF_NODE_CELL_SIZE: 297",
-      "LEAF_NODE_SPACE_FOR_CELLS: 4086",
-      "LEAF_NODE_MAX_CELLS: 13",
-      "db > ",
-    ])
-  end
-
-  it 'allows printing out the structure of a one-node btree' do
-    script = [3, 1, 2].map do |i|
-      "insert #{i} user#{i} person#{i}@example.com"
-    end
-    script << ".btree"
-    script << ".exit"
-    result = run_script(script)
-
-    expect(result).to eq([
-      "db > Executed.",
-      "db > Executed.",
-      "db > Executed.",
-      "db > Tree:",
-      "leaf (size 3)",
-      "  - 0 : 1",
-      "  - 1 : 2",
-      "  - 2 : 3",
-      "db > "
-    ])
-  end
-
   it 'prints an error message if there is a duplicate id' do
     script = [
       "insert 1 user1 person1@example.com",
@@ -166,4 +117,35 @@ describe 'database' do
       "db > ",
     ])
   end
+
+  it 'prints all rows in a multi-level tree' do
+    script = []
+    (1..15).each do |i|
+      script << "insert #{i} user#{i} person#{i}@example.com"
+    end
+    script << "select"
+    script << ".exit"
+    result = run_script(script)
+  
+    expect(result[15...result.length]).to match_array([
+      "db > (1, user1, person1@example.com)",
+      "(2, user2, person2@example.com)",
+      "(3, user3, person3@example.com)",
+      "(4, user4, person4@example.com)",
+      "(5, user5, person5@example.com)",
+      "(6, user6, person6@example.com)",
+      "(7, user7, person7@example.com)",
+      "(8, user8, person8@example.com)",
+      "(9, user9, person9@example.com)",
+      "(10, user10, person10@example.com)",
+      "(11, user11, person11@example.com)",
+      "(12, user12, person12@example.com)",
+      "(13, user13, person13@example.com)",
+      "(14, user14, person14@example.com)",
+      "(15, user15, person15@example.com)",
+      "Executed.", "db > ",
+    ])
+  end
+  
+  
 end
